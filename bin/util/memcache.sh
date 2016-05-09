2e75a2040ade40f76d6966de6a8beae9df7f8da1
Trương Phong <programmingd32@gmail.com>
2016-05-09 16:29:14 +0700

    create memcache extension

---
+++ bin/util/memcache.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Build Path: /app/.heroku/php/
+
+dep_url = git://github.com/hungphongbk/memcache.git
+$memcache_dir = memcache
+echo "-----> Building Memcache..."
+
+### Memcache
+echo "[LOG] Downloading Memcache"
+git clone $dep_url -q
+if [ ! -d "$memcache_dir" ]; then
+  echo "[ERROR] Failed to find Memcache directory $memcache_dir"
+  exit
+fi
+cd $memcache
+
+BUILD_DIR=$1
+ln -s $BUILD_DIR/.heroku /app/.heroku
+export PATH=/app/.heroku/php/bin:$PATH
+phpize
+./configure --with-php-config=$PHP_ROOT/bin/php-config
+make
+make install
+cd
+echo "important extension memcache into php.ini"
+echo "extension=memcache.so" >> /app/.heroku/php/etc/php/php.ini
